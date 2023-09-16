1bdb6c6a0888a49a337a15d61dc9f4583a3e566f
Amir Dirin <33372535+ADirin@users.noreply.github.com>
2023-09-16 15:47:58 +0300

    Add files via upload

---
+++ Week 5/MavenJacocoTest/fi_maven_test/src/main/java/fi_maven_test/CalCulate.java
@@ -0,0 +1,22 @@
+package fi_maven_test;
+
+public class CalCulate {
+	
+	public static int laskeSumma(int a, int b) {
+		
+		return a+b;
+	}
+	
+public static int laskeMinnus(int a, int b) {
+		
+		return a-b;
+	}
+
+public static int laskeKerta(int a, int b) {
+	
+	return a*b;
+}
+
+	
+	
+}
